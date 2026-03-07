063277497951079de71d9852118a2501ce2ecd16
Manan Kush <Manankush@icloud.com>
2026-03-07 13:55:59 +0530

    changes to env and compose

--- backend/main.py
+++ backend/main.py
@@ -37,6 +37,8 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:3000",
         "http://127.0.0.1:5173",
+        "http://192.168.0.132:3000",
+        "http://192.168.0.132:5173",
         os.getenv("FRONTEND_URL", "http://localhost:3000"),
     ],
     allow_credentials=True,
